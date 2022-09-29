eabcc5fa0bcc7ee93a4a7bbc5dac3be8f8ded679
JonathanFcosta17 <jonathanfc2010@gmail.com>
2022-09-29 14:55:28 -0300

    Initial commit

--- main.py
+++ main.py
@@ -6,17 +6,25 @@ print("Enter a list of options separated by commas")
 print("Example: 'Option 1, Option 2, Option 3'")
 options = input("Enter your options: ")
 
-decision = DecisionMaker(options)  # Create a decision maker object
+# Create a decision maker object
+decision = DecisionMaker(options)
 
+# Asking if the user wants to add weights
 print("\nWould you like to add some weight to the options?")
 print("Enter 'y' for yes and 'n' for no")
+
+# Looping until the user enters a valid answer
 while True:
     answer = input("Enter your choice: ").lower()
+
+    # If the user doesn't want to add weights
     if answer == 'n':
         print("\nYou asked The Great Decision Maker what to do.")
         print("The Great Decision Maker says: " +
               decision.make_decision_simple())
         break
+
+    # If the user wants to add weights
     elif answer == 'y':
         print(f"This is the list of options that you entered: {options}")
         weights = input("Enter the weights: ")
